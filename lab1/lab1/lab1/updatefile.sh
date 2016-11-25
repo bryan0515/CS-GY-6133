084a94fdc67cd7b6099bc7e0312ad3d271770a77
bryan0515 <bc2197@nyu.edu>
2016-11-25 17:45:50 -0500

    Delete updatefile.sh

--- lab1/lab1/lab1/updatefile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-scp ./MIPS.cpp bc2197@gauss.poly.edu:/home/FALL2016/bc2197/CA
-scp ./dmem.txt bc2197@gauss.poly.edu:/home/FALL2016/bc2197/CA
-scp ./imem.txt bc2197@gauss.poly.edu:/home/FALL2016/bc2197/CA
